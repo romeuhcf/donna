01e40f6ebe2cb207ab2b6da94283cc6030fb4633
Romeu Fonseca <romeu.hcf@gmail.com>
2018-03-26 17:22:35 -0300

    turns off csrf on wakeup

--- app/controllers/wakeup_controller.rb
+++ app/controllers/wakeup_controller.rb
@@ -1,5 +1,5 @@
 class WakeupController < ApplicationController
-  skip_before_filter :verify_authenticity_token, :only => [:index]
+  skip_before_action :verify_authenticity_token, :only => [:index]
   def index
     Rails.logger.warn "Wakeup running"
   end
