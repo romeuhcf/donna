3b7a0677553668108bfc7cd400cb9e2b53b6926d
Romeu Fonseca <romeu.hcf@gmail.com>
2018-03-26 17:15:55 -0300

    updates router

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :contracts
   resources :payment_modes
+  post '/_wakeup', to: 'wakeup#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
